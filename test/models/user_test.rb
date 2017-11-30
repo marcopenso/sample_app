54da3d6532aa15c882df9247c4ead00f2c9b45ef
Marco <33624820+marcopenso@users.noreply.github.com>
2017-11-30 17:15:57 +0100

    6.2.5 Uniqueness validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -48,4 +48,18 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test "email should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    duplicate_user.email = @user.email.upcase
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
 end
